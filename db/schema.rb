a670d5cc436f0d8f5933705991bc139972faf254
stangar <simon.tang.arias@gmail.com>
2016-07-25 18:54:12 -0400

    d

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725204355) do
+ActiveRecord::Schema.define(version: 20160725205300) do
 
   create_table "cars", force: :cascade do |t|
     t.string   "make"
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 20160725204355) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "current_state"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",            null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
