b35f85a77047438a0365c4797403c20c921ccfc6
Leon Chiu <leontkchiu@gmail.com>
2016-07-28 16:08:47 -0400

    rebuilt header

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -2,21 +2,23 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 $(function() {
-   $('#client_client_id').on('change',function() {
+  $('#client_client_id').on('change',function() {
     console.log($(this).val());
-   $.ajax({
-       url: '/clients/' + $(this).val(),
-       method: 'GET',
-       data: {},
-       dataType: 'json'
-     }).done(function(responseData) {
-       $('#job_car_id').html("")
-       for (var i = 0; i < responseData.length; i++) {
-         var CarItem =  $('<option class="car"></option>');
-         CarItem.attr('value', responseData[i].id);
-         CarItem.html(responseData[i].model)
-         $('#job_car_id').append(CarItem)
-       }
-     });
-   });
+    $('.carform').css("display","block")
+    $.ajax({
+      url: '/clients/' + $(this).val(),
+      method: 'GET',
+      data: {},
+      dataType: 'json'
+    }).done(function(responseData) {
+      $('#job_car_id').html("")
+      for (var i = 0; i < responseData.length; i++) {
+        var CarItem =  $('<option class="car"></option>');
+        CarItem.attr('value', responseData[i].id);
+        CarItem.html(responseData[i].model)
+        $('#job_car_id').append(CarItem)
+      }
+    });
+    $('#newcar').attr("href",'/clients/' + $(this).val() + '/cars/new')
+  });
 });
