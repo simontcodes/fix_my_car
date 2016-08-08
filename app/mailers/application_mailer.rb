e92c0518e453a7fd5ec1d651f37b3df36a731643
stangar <simon.tang.arias@gmail.com>
2016-08-08 18:44:44 -0400

    trying sendgrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "#{Figaro.env.gmail_username }@gmail.com"
+  # default from: "#{Figaro.env.gmail_username }@gmail.com"
+  default from: "fixmycar.bitmaker@gmail.com"
   #takes the gmail username from  the figaro file
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -83,4 +84,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "https://fixmycarbitmaker.herokuapp.com/" }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
