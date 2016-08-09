0e14ae8761d52a6e9be490f898f204e737fdc656
stangar <simon.tang.arias@gmail.com>
2016-08-09 17:21:43 -0400

    corrected spelling

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,7 +4,7 @@ class Client < ApplicationRecord
 
 
   validates :name, :email, :phone, presence: true
-  validates :email, uniqueness: true
+  # validates :email, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 
 end
