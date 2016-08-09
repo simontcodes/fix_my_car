e5b086928d7ca4caf11bab5127abb3b176b49703
stangar <simon.tang.arias@gmail.com>
2016-08-09 16:10:15 -0400

    sending aproval message

--- app/models/status.rb
+++ app/models/status.rb
@@ -12,8 +12,8 @@ class Status < ApplicationRecord
     elsif self.job.budgeting?
       self.job.approve!
       ClientMailer.aproval(self.job.car.client, self.job).deliver_later
-      send_message("+14162007371","Thank you for choosing FixMyCar, a cost estimate has been uploaded go to the link to download it.
-      #{job_url(id: self.job.token)}")
+      # send_message("+14162007371","Thank you for choosing FixMyCar, a cost estimate has been uploaded go to the link to download it.
+      # #{job_url(id: self.job.token)}")
     elsif self.job.waiting_for_approval?
       self.job.repair!
     elsif self.job.repairing?
