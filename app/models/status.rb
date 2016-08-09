412ca03b34f47ab647d3632f89e8f86b8fdd570a
stangar <simon.tang.arias@gmail.com>
2016-08-09 16:02:26 -0400

    sending aproval message

--- app/models/status.rb
+++ app/models/status.rb
@@ -12,7 +12,7 @@ class Status < ApplicationRecord
     elsif self.job.budgeting?
       self.job.approve!
       ClientMailer.aproval(self.job.car.client, self.job).deliver_later
-      self.job.send_message("+14162007371","Thank you for choosing FixMyCar, a cost estimate has been uploaded go to the link to download it.
+      send_message("+14162007371","Thank you for choosing FixMyCar, a cost estimate has been uploaded go to the link to download it.
       #{job_url(id: @job.token)}")
     elsif self.job.waiting_for_approval?
       self.job.repair!
@@ -22,5 +22,27 @@ class Status < ApplicationRecord
     end
   end
 
+  def send_message(phone_number, alert_message)
+  # account_sid = "AC5bb068e864539b01f20f046a8635c262"
+    #test auth_token = "189c79a9541f497d17192574166f5c53"
+    # account_sid = Rails.application.secrets.twilio_key
+    # auth_token = Rails.application.secrets.twilio_auth
+
+    account_sid = ENV['TWILIO_KEY']
+    auth_token = ENV['TWILIO_AUTH']
+
+      # binding.pry
+       twilio_number = "+16474964559"
+       client = Twilio::REST::Client.new account_sid, auth_token
+
+       message = client.account.messages.create(
+         :from => twilio_number,
+         :to => phone_number,
+         :body => alert_message,
+         # US phone numbers can make use of an image as well.
+         # :media_url => image_url
+       )
+       puts message.to
+   end
 
 end
